c7c0e7e3bccc8bf189a927f1c8cbf16d2441a7c8
Lucas Soares de Brito <ext-lucas.brito@bbts.com.br>
2021-04-24 11:20:07 -0300

    inicia mudança de design e troca de nomes

--- components/Header.js
+++ components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <div>
       <h1 className={headerStyles.title}>
-        <span>WebDev</span> News
+        <span>Lucas Aguiar</span> Agência de Marketing
       </h1>
       <p className={headerStyles.description}>
         Keep up to date with the latest web dev news
