df0c860a45c1298c181232a8f1e8c644a007baf9
Lucas Soares de Brito <ext-lucas.brito@bbts.com.br>
2021-04-24 11:05:40 -0300

    retira server env

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -18,39 +18,9 @@ const article = ({article}) => {
     )
 }
 
-export const getStaticProps = async (context) => {
-    const res = await fetch(
-        `${server}/api/articles/${context.params.id}`
-    )
-
-    const article = await res.json()
-
-    return {
-        props: {
-            article,
-        },
-    }
-}
-
-export const getStaticPaths = async () => {
-    const res = await fetch(
-        `${server}/api/articles`
-    )
-
-    const articles = await res.json()
-
-    const ids = articles.map((article) => article.id)
-    const paths = ids.map((id) => ({params: {id: id.toString()}}))
-
-    return {
-        paths,
-        fallback: false
-    }
-}
-
 // export const getStaticProps = async (context) => {
 //     const res = await fetch(
-//         `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+//         `${server}/api/articles/${context.params.id}`
 //     )
 
 //     const article = await res.json()
@@ -64,7 +34,7 @@ export const getStaticPaths = async () => {
 
 // export const getStaticPaths = async () => {
 //     const res = await fetch(
-//         `https://jsonplaceholder.typicode.com/posts/`
+//         `${server}/api/articles`
 //     )
 
 //     const articles = await res.json()
@@ -77,5 +47,35 @@ export const getStaticPaths = async () => {
 //         fallback: false
 //     }
 // }
+
+export const getStaticProps = async (context) => {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/${context.params.id}`
+    )
+
+    const article = await res.json()
+
+    return {
+        props: {
+            article,
+        },
+    }
+}
+
+export const getStaticPaths = async () => {
+    const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts/`
+    )
+
+    const articles = await res.json()
+
+    const ids = articles.map((article) => article.id)
+    const paths = ids.map((id) => ({params: {id: id.toString()}}))
+
+    return {
+        paths,
+        fallback: false
+    }
+}
 
 export default article
--- pages/index.js
+++ pages/index.js
@@ -9,22 +9,9 @@ export default function Home({ articles }) {
   );
 }
 
-export const getStaticProps = async () => {
-  const res = await fetch(
-    `${server}/api/articles`
-  );
-  const articles = await res.json();
-
-  return {
-    props: {
-      articles,
-    },
-  };
-};
-
 // export const getStaticProps = async () => {
 //   const res = await fetch(
-//     `https://jsonplaceholder.typicode.com/posts?_limit=6`
+//     `${server}/api/articles`
 //   );
 //   const articles = await res.json();
 
@@ -34,3 +21,16 @@ export const getStaticProps = async () => {
 //     },
 //   };
 // };
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
